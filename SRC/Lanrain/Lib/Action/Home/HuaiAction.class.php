655a112e67275b4e5168390ac4efa8e156d4c7eb
ranyigit <ranyi_num@163.com>
2014-10-20 16:20:32 +0800

    配置文件修改及资讯

---
+++ SRC/Lanrain/Lib/Action/Home/HuaiAction.class.php
@@ -0,0 +1,52 @@
+<?php
+class HuaiAction extends BaseAction{
+	
+	
+	//资讯
+	public function news(){
+		/*$db=D('Img');	
+		if($_GET['p']==false){
+			$page=1;
+		}else{
+			$page=$_GET['p'];			
+		}		
+		$where['classid']=$this->_get('classid','intval');
+		$count=$db->where($where)->count();	
+		$pageSize=8;	
+		$pagecount=ceil($count/$pageSize);
+		if($page > $count){$page=$pagecount;}
+		if($page >=1){$p=($page-1)*$pageSize;}
+		if($p==false){$p=0;}
+		$res=$db->where($where)->order('statdate DESC,id DESC')->limit("{$p},".$pageSize."")->select();
+		$res=$this->convertLinks($res);
+		
+		$this->assign('page',$pagecount);
+		$this->assign('p',$page);
+		$this->assign('info',$this->info);
+		$this->assign('tpl',$this->tpl);
+		$this->assign('res',$res);
+		$this->assign('copyright',$this->copyright);
+		if ($count==1){
+		    if($res[0]['url'] != ''){
+		       header("location:".$res[0]['url']);
+		    }
+			$this->content($res[0]['id']);
+			exit();
+		}
+		//$this->display($this->tpl['tpllistname']);
+*/
+		  $this->display();
+	}
+	
+	public function convertLinks($arr){
+		$i=0;
+		foreach ($arr as $a){
+			if ($a['url']){
+				$arr[$i]['url']=$this->getLink($a['url']);
+			}
+			$i++;
+		}
+		return $arr;
+	}
+}
+?>
--- SRC/data/conf/Home/config.php
+++ SRC/data/conf/Home/config.php
@@ -2,4 +2,7 @@
 return array(
 	'TMPL_FILE_DEPR'=>'_',
 	'DEFAULT_THEME'=>'lanrain',
+	'TMPL_PARSE_STRING' =>array(
+	    '__PUBLIC__' =>  __ROOT__.'/tpl/Home/lanrain/common',
+	)
 );
