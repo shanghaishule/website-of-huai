eff50850a52bd7a4a5b927b6ee3076a7c795dca2
ranyigit <ranyi_num@163.com>
2014-10-21 14:59:30 +0800

    test

--- SRC/Lanrain/Lib/Action/Home/HuaiAction.class.php
+++ SRC/Lanrain/Lib/Action/Home/HuaiAction.class.php
@@ -1,4 +1,8 @@
 <?php
+function strExists($haystack, $needle)
+{
+	return !(strpos($haystack, $needle) === FALSE);
+}
 class HuaiAction extends BaseAction {
 	private $info;
 	public function _initialize(){
@@ -46,17 +50,13 @@ class HuaiAction extends BaseAction {
 		}else{
 			$page=$_GET['p'];
 		}
-		//$where['classid']=$this->_get('classid','intval');
-		$where['classname']=$this->_get('classname','trim');
-		$count=$db->where($where)->count();
-		$pageSize=8;
-		$pagecount=ceil($count/$pageSize);
-		if($page > $count){$page=$pagecount;}
-		if($page >=1){$p=($page-1)*$pageSize;}
-		if($p==false){$p=0;}
-		$res=$db->where($where)->order('statdate DESC,id DESC')->limit("{$p},".$pageSize."")->select();
+		//$where['classname']=$this->_get('classname','trim');
+		$count=$db->count();
+		$Page       = new Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数
+		$show       = $Page->show();//
+		$res=$db->order('statdate DESC,id DESC')->limit($Page->firstRow.','.$Page->listRows)->select();
 		$res=$this->convertLinks($res);
-		$this->assign('page',$pagecount);
+		$this->assign('page',$show);
 		$this->assign('p',$page);
 		$this->assign('info',$this->info);
 		$this->assign('tpl',$this->tpl);
@@ -79,5 +79,79 @@ class HuaiAction extends BaseAction {
 		}
 		return $arr;
 	}
+	public function getLink($url){
+		$urlArr=explode(' ',$url);
+		$urlInfoCount=count($urlArr);
+		if ($urlInfoCount>1){
+			$itemid=intval($urlArr[1]);
+		}
+		//会员卡 刮刮卡 团购 商城 大转盘 优惠券 订餐 商家订单 表单
+		if (strExists($url,'刮刮卡')){
+			$link='/index.php?g=Wap&m=Guajiang&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+			if ($itemid){
+				$link.='&id='.$itemid;
+			}
+		}elseif (strExists($url,'大转盘')){
+			$link='/index.php?g=Wap&m=Lottery&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+			if ($itemid){
+				$link.='&id='.$itemid;
+			}
+		}elseif (strExists($url,'砸金蛋')){
+			$link='/index.php?g=Wap&m=Zadan&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+			if ($itemid){
+				$link.='&id='.$itemid;
+			}
+		}elseif (strExists($url,'优惠券')){
+			$link='/index.php?g=Wap&m=Coupon&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+			if ($itemid){
+				$link.='&id='.$itemid;
+			}
+		}elseif (strExists($url,'刮刮卡')){
+			$link='/index.php?g=Wap&m=Guajiang&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+			if ($itemid){
+				$link.='&id='.$itemid;
+			}
+		}elseif (strExists($url,'商家订单')){
+			if ($itemid){
+				$link=$link='/index.php?g=Wap&m=Host&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id.'&hid='.$itemid;
+			}else {
+				$link='/index.php?g=Wap&m=Host&a=Detail&token='.$this->token.'&wecha_id='.$this->wecha_id;
+			}
+		}elseif (strExists($url,'万能表单')){
+			if ($itemid){
+				$link=$link='/index.php?g=Wap&m=Selfform&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id.'&id='.$itemid;
+			}
+		}elseif (strExists($url,'相册')){
+			$link='/index.php?g=Wap&m=Photo&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+			if ($itemid){
+				$link='/index.php?g=Wap&m=Photo&a=plist&token='.$this->token.'&wecha_id='.$this->wecha_id.'&id='.$itemid;
+			}
+		}elseif (strExists($url,'全景')){
+			$link='/index.php?g=Wap&m=Panorama&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+			if ($itemid){
+				$link='/index.php?g=Wap&m=Panorama&a=item&token='.$this->token.'&wecha_id='.$this->wecha_id.'&id='.$itemid;
+			}
+		}elseif (strExists($url,'会员卡')){
+			$link='/index.php?g=Wap&m=Card&a=vip&token='.$this->token.'&wecha_id='.$this->wecha_id;
+		}elseif (strExists($url,'商城')){
+			$link='/index.php?g=Wap&m=Product&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+		}elseif (strExists($url,'订餐')){
+			$link='/index.php?g=Wap&m=Product&a=dining&dining=1&token='.$this->token.'&wecha_id='.$this->wecha_id;
+		}elseif (strExists($url,'团购')){
+			$link='/index.php?g=Wap&m=Groupon&a=grouponIndex&token='.$this->token.'&wecha_id='.$this->wecha_id;
+		}elseif (strExists($url,'留言')){
+			$link='/index.php?g=Wap&m=Liuyan&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+		}elseif (strExists($url,'首页')){
+			$link='/index.php?g=Wap&m=Index&a=index&token='.$this->token.'&wecha_id='.$this->wecha_id;
+		}else {
+			if (strpos($url,'?')){
+				$link=str_replace('{wechat_id}',$this->wecha_id,$url).'&wecha_id='.$this->wecha_id;
+			}else {
+				$link=str_replace('{wechat_id}',$this->wecha_id,$url).'?wecha_id='.$this->wecha_id;
+			}
+				
+		}
+		return $link;
+	}	
 }
 ?>
