920e33668e86922e396f59769d6dd362934c6c0c
ranyigit <ranyi_num@163.com>
2014-12-11 15:11:12 +0800

    404页面

--- SRC/Lanrain/Lib/Action/BaseAction.class.php
+++ SRC/Lanrain/Lib/Action/BaseAction.class.php
@@ -10,7 +10,7 @@ class BaseAction extends Action
         $this->getWechaId();
     }
 
-    function _empty(){
+   public function _empty(){
     	header("HTTP/1.0 404 Not Found");//使HTTP返回404状态码
     	$this->display("Public:404");
     }
