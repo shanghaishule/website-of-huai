4d02677c959c76eb0bf94d67fc9e7a32de196cc0
ranyigit <ranyi_num@163.com>
2014-12-11 15:39:54 +0800

    测试

--- SRC/Lanrain/Lib/Action/BaseAction.class.php
+++ SRC/Lanrain/Lib/Action/BaseAction.class.php
@@ -11,7 +11,7 @@ class BaseAction extends Action
     }
 
    public function _empty(){
-    	header("HTTP/1.0 404 Not Found");//使HTTP返回404状态码
+    	//header("HTTP/1.0 404 Not Found");//使HTTP返回404状态码
     	$this->display("Public:404");
     }
     
