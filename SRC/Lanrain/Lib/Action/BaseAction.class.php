bce5463594747172d7e7b457a9f55f07a629a3f9
ranyigit <ranyi_num@163.com>
2014-12-11 15:27:57 +0800

    测试

--- SRC/Lanrain/Lib/Action/BaseAction.class.php
+++ SRC/Lanrain/Lib/Action/BaseAction.class.php
@@ -11,6 +11,7 @@ class BaseAction extends Action
     }
 
    public function _empty(){
+   		dump('ceshi');die;
     	header("HTTP/1.0 404 Not Found");//使HTTP返回404状态码
     	$this->display("Public:404");
     }
